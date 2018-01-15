482cbe97b0fc20daaa00b7607a6f092c7faea538
liuuu <next@yudeMacBook-Air.local>
2018-01-15 22:20:28 +0800

    basic feature

--- src/App.js
+++ src/App.js
@@ -1,66 +1,106 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
-
 import './App.css';
 import { observable } from 'mobx';
+import { inject } from 'mobx-react';
+import DevTools from 'mobx-react-devtools';
+import styled from 'styled-components';
 
-@observer
+const CartItem = observer(({ item, addNum, check, del }) => {
+  return (
+    <div
+      className="cart-item"
+      style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+    >
+      <div>
+        <label>
+          <input type="checkbox" checked={item.checked} onChange={() => check(item.id)} />
+        </label>
+      </div>
+      <div>{item.name}</div>
+      <div>{item.price}</div>
+      <ul>
+        <li onClick={() => addNum(item.id, 'minus')}>-</li>
+        <li>{item.num}</li>
+        <li onClick={() => addNum(item.id, 'add')}>+</li>
+      </ul>
+      <div>小计{item.price * item.num}</div>
+
+      <div onClick={() => del(item.id)}>删除</div>
+    </div>
+  );
+});
+
+const AllItems = inject(['storeState'])(
+  observer(({ storeState }) => {
+    return storeState.items.map(item => (
+      <CartItem
+        key={item.id}
+        item={item}
+        check={storeState.check}
+        addNum={storeState.addNum}
+        del={storeState.del}
+      />
+    ));
+  })
+);
+
+const Header = inject(['storeState'])(
+  observer(props => (
+    <Wrapper>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={props.storeState.checkedAll}
+            onChange={props.storeState.handleCheckall}
+          />
+          全选
+        </label>
+      </div>
+      <div>商品</div>
+      <div>单价</div>
+      <div>数量</div>
+      <div>小计</div>
+      <div>删除</div>
+    </Wrapper>
+  ))
+);
+
+const Footer = inject(({ storeState }) => ({
+  total: storeState.total,
+}))(
+  observer(props => {
+    console.log('props', props);
+
+    return <div className="footer">总计: {props.total}</div>;
+  })
+);
+
 class App extends Component {
-  componentWillUpdate() {
-    console.log('will update');
-  }
-
-  handlePress = e => {
-    if (e.keyCode === 13) {
-      console.log('e', e);
-      this.props.store.createTodo(e.target.value);
-      e.target.value = '';
-    }
-  };
-
-  handleFilter = e => {
-    this.props.store.filter = e.target.value;
-  };
-
-  toggleComplete = item => {
-    item.completed = !item.completed;
-  };
-
-  showCompleted = () => {
-    this.props.store.showAll = !this.props.store.showAll;
-  };
-
-  handleClear = () => {
-    this.props.store.handleClear();
-  };
-
   render() {
-    const { filteredTodos, showAll } = this.props.store;
-
     return (
       <div>
-        <input placeholder="add new todo" onKeyUp={this.handlePress} />
-        <input placeholder="filter todos" onKeyUp={this.handleFilter} />
-        <div>{showAll ? 'All' : 'Only Completed'}</div>
-        <ul>
-          {filteredTodos.map((item, index) => {
-            return (
-              <li key={index}>
-                <input
-                  type="checkbox"
-                  checked={item.completed}
-                  onChange={() => this.toggleComplete(item)}
-                />
-                {item.name}
-              </li>
-            );
-          })}
-        </ul>
-        <button onClick={this.showCompleted}>{showAll ? 'show competed' : 'show all'}</button>
-        <button onClick={this.handleClear}>clear completed</button>
+        <h1>展示</h1>
+        <Header />
+        <AllItems />
+        <Footer />
+        <DevTools />
       </div>
     );
   }
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  div {
+    width: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import todoStore from './todoStore';
+import storeState from './store.js';
+import { Provider } from 'mobx-react';
+import mobx from 'mobx';
 
-ReactDOM.render(<App store={todoStore} />, document.getElementById('root'));
+const stores = { storeState };
+ReactDOM.render(
+  <Provider {...stores}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
